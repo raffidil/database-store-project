60bb42f60515917ccd5fdec715d9600a5da9bb6e
Raffi Dilanchian <raffidilanchian@gmail.com>
2019-04-26 19:54:27 +0430

    Create blank tables in db.

--- store.py
+++ store.py
@@ -1,5 +1,22 @@
 import sqlite3
 
-
+# create connection
 connection = sqlite3.connect('database.db')
-c = connection.cursor()
+db = connection.cursor()
+
+# initialize:
+db.execute(
+    """CREATE TABLE IF NOT EXISTS Item (Id number PRIMARY KEY, Name text, Price number)""")
+db.execute(
+    """CREATE TABLE IF NOT EXISTS Staff (Id number PRIMARY KEY, Name text)""")
+db.execute(
+    """CREATE TABLE IF NOT EXISTS Warehouse (Id number PRIMARY KEY, Mid number, City text)""")
+db.execute(
+    """CREATE TABLE IF NOT EXISTS Stock (Iid number ,Wid number, Quantity number)""")
+
+# code
+
+
+# close connection
+connection.commit()
+connection.close()
