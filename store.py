32a2d89c9d6291c95e926f7953c233aecef5078e
Raffi Dilanchian <raffidilanchian@gmail.com>
2019-04-29 19:58:35 +0430

    Add command menu.

--- store.py
+++ store.py
@@ -17,6 +17,49 @@ db.execute(
 # code
 
 
+def insert_staff(id, name):
+    db.execute(
+        """INSERT INTO Staff (Id, Name) VALUES (?,?)""", (id, name))
+
+
+def insert_item(id, name, price):
+    db.execute(
+        """INSERT INTO Item (Id, Name, Price) VALUES (?,?,?)""", (id, name, price))
+
+
+def insert_warehouse(id, mid, city):
+    db.execute(
+        """INSERT INTO Warehouse (Id, Mid, City) VALUES (?,?,?)""", (id, mid, city))
+
+
+def insert_stock(iid, wid, quantity):
+    db.execute(
+        """INSERT INTO Warehouse (Iid, Wid, Quantity) VALUES (?,?,?)""", (iid, wid, quantity))
+
+
+def print_command():
+    a = input("""
+        1.Insert Staff
+        2.Insert Item
+        3.Insert Warehouse
+        4.Insert Stock
+        0.exit\n
+        command: """)
+    return a
+
+
+# main function
+_command = print_command()
+
+
+while _command is not '0':
+    if _command == '1':
+        _staff_id = input("Input Staff ID: ")
+        _staff_name = input("Input Staff Name: ")
+        insert_staff(_staff_id, _staff_name)
+    _command = print_command()
+
+
 # close connection
 connection.commit()
 connection.close()
